7b01b9cceadd39c63e8bb87935cd150ee73e6e2f
Vladimir Shitov <vladimirshitov98@gmail.com>
2022-09-08 19:38:19 +0600

    Move functions to utils

---
+++ utils.js
@@ -0,0 +1,104 @@
+//------------RANDOM-------------
+function randomUniform(low, high) {
+    var u = Math.random() * (high - low);
+    return u + low;
+}
+function randomChoice(array){
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+//------------DRAWING-------------
+function circle(x, y, radius, fillCircle){
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI*2);
+    if (fillCircle){
+        ctx.fill();
+    } else {
+        ctx.stroke();
+    }
+}
+function square(x, y, size){
+    ctx.style = "black";
+    ctx.fillRect(x, y, size, size);
+    ctx.strokeRect(x, y, size, size);
+}
+function star(cx, cy, nSpikes, outerRadius, innerRadius, color){
+      var rot=Math.PI/2*3;
+      var x=cx;
+      var y=cy;
+      var step=Math.PI/nSpikes;
+
+      ctx.beginPath();
+      ctx.moveTo(cx, cy-outerRadius)
+      for(i=0;i<nSpikes;i++){
+        x=cx+Math.cos(rot)*outerRadius;
+        y=cy+Math.sin(rot)*outerRadius;
+        ctx.lineTo(x,y)
+        rot+=step
+
+        x=cx+Math.cos(rot)*innerRadius;
+        y=cy+Math.sin(rot)*innerRadius;
+        ctx.lineTo(x,y)
+        rot+=step
+      }
+      ctx.lineTo(cx,cy-outerRadius);
+      ctx.closePath();
+      ctx.lineWidth=1;
+      ctx.strokeStyle='black';
+      ctx.stroke();
+      ctx.fillStyle=color;
+      ctx.fill();
+    }
+
+//--------OTHER SUPPORT----------
+function clip(x, min, max) {
+    return Math.min(Math.max(min, x), max);
+}
+
+function moveTo(xFrom, yFrom, xTo, yTo, speed){
+    if (xTo === xFrom && yTo === yFrom){
+        return [0, 0];
+    } 
+    var x_sign = (xTo - xFrom)/Math.abs(xTo - xFrom);
+    var y_sign = (yTo - yFrom)/Math.abs(yTo - yFrom);
+    var ratio = (yTo - yFrom)/(xTo - xFrom);
+
+    // Real calculation: speed is equal to base speed, direction is 'to the xTo yTo'
+    var xSpeed;
+    var ySpeed;
+    if (Math.abs(xTo - xFrom) < 1){xSpeed = 0;} else {xSpeed = x_sign * speed / Math.sqrt(ratio*ratio + 1)}
+    if (Math.abs(yTo - yFrom) < 1){ySpeed = 0;} else {ySpeed = y_sign * Math.abs(ratio * speed)} 
+    return [xSpeed, ySpeed];
+}
+
+//-------SUPPORT FOR CELLS-------
+function findTarget(x, y, targetList, n) {
+    if (targetList.length > 0){
+        var number = Math.floor(randomUniform(0, n));
+        var res = targetList.sort(function(a, b){
+            var da = Math.abs(Math.pow(x - a.x, 2) + Math.pow(y - a.y, 2));
+            var db = Math.abs(Math.pow(x - b.x, 2) + Math.pow(y - b.y, 2));
+            return da-db;
+        })[number]
+        return res;            
+    } else {return -1;}
+}
+function findRandomTarget(targetsList) {
+    if (targetsList.length > 0){
+        var idx = Math.round(randomUniform(0, targetsList.length));
+        return targetsList[idx];
+    }
+    else {
+        return -1;
+    }
+}
+function doCirclesIntersect(x1, y1, r1, x2, y2, r2) {
+    centersDistance = Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+    return (centersDistance <= Math.pow(r1 + r2, 2));
+}
+function tissueCellsDistance(c1, c2){
+    return (Math.abs(c1.x - c2.x) + Math.abs(c1.y-c2.y))/(tissueCellSize + spaceBetweenTissueCells);
+}
