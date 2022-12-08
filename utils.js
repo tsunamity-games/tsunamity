7e1c5126b61c2465a8a43f5536be11b2d91ad818
Dmitry Biba <dmitriy.biba@gmail.com>
2022-12-08 14:42:31 +0300

    tutorial highlighting with color

--- utils.js
+++ utils.js
@@ -114,8 +114,12 @@ function drawBlackScreen(alpha, highlight_x, highlight_y, highlight_width, highl
     ctx.fillStyle = "white";
     roundRect(ctx, highlight_x, highlight_y, highlight_width, highlight_height,
          leftRadius=radius, rightRadius=radius, fill=true);
-
     ctx.globalAlpha = 1;
+    ctx.strokeStyle = "#0099ff";
+    ctx.lineWidth = 5;
+    roundRect(ctx, highlight_x, highlight_y, highlight_width, highlight_height,
+         leftRadius=radius, rightRadius=radius, fill=false, stroke=true);
+    
 }
 
 //--------OTHER SUPPORT----------
